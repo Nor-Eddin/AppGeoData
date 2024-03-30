934540cb19a7d48736a84dc01fa934b60fb298c6
Nor-Eddin <noreddinlam@gmail.com>
2024-03-30 18:05:36 +0100

    mise en place de la modal d'info

--- src/components/ListCard.jsx
+++ src/components/ListCard.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import ModalContent from './ModalContent';
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
 
-export default function ListCard({country}) {
-  return (
-    <li className="relative cursor-pointer rounded transition-transform duration-300 hover:-translate-y-1 will-change-transform">
-        <h4 className="absolute left-0 top-0 p-1 bg-gray-100 drop-shadow-[2px_2px_1px_rgba(0,0,0,0.5)] rounded"> {country.name.common}</h4>
-        <img className="w-full h-full object-cover rounded" src={country.flags.svg} alt="" />
-    </li>
-  )
+export default function ListCard({ country }) {
+    const [showModal, setShowModal] = useState(false);
+    return (
+        <>
+            <li onClick={()=>setShowModal(!showModal)} className="relative cursor-pointer rounded transition-transform duration-300 hover:-translate-y-1 will-change-transform">
+
+                <h4 className="absolute left-0 top-0 p-1 bg-gray-100 drop-shadow-[2px_2px_1px_rgba(0,0,0,0.5)] rounded"> {country.name.common}</h4>
+                <img className="w-full h-full object-cover rounded" src={country.flags.svg} alt="" />
+            </li>
+            {showModal && createPortal(<ModalContent country={country} closeModal={()=>setShowModal(!showModal)}/>,document.body)}
+        </>
+    )
 }
